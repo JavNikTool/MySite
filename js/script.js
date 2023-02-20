dbe34705f1e9af45857dbaec28a0d6b9ae6b1b24
JavNikTool <Nikitakremnev0@gmail.com>
2023-02-20 12:55:27 +0300

    v0.004

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("проверк22");
+$(document).ready(function(){
+   $('.button_aside').click(function (){
+       $('.hiden_nav').addClass('hiden_nav_active');
+       $(this).addClass('button_aside_active');
+   });
+    $('.exit').click(function (){
+        $('.hiden_nav').removeClass('hiden_nav_active');
+        $('.button_aside').removeClass('button_aside_active');
+    });
+    // if($(".hiden_nav ul li a") == event.target(onclick())){
+    //     $('.hiden_nav').addClass('hiden_nav_active');
+    // }
+});
