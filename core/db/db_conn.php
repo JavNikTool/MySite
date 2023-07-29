d71140c2966680c853a8d3da3c2b3e32c75dd0cf
JavNikTool <91914854+JavNikTool@users.noreply.github.com>
2023-07-29 17:08:32 +0300

    Delete db_conn.php

--- core/db/db_conn.php
+++
@@ -1,6 +0,0 @@
-<?php
-try {
-    $conn = new PDO('pgsql:host=localhost;dbname=s1', 'javniktool', 'dbtest');
-}catch (PDOException $e){
-    echo "ошибка подключения к базе данных " . $e->getMessage();
-}
