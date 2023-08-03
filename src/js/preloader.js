faf81264ad6a5264ff12aa0ac2eed76ee7eb9bed
JavNikTool <Nikitakremnev0@gmail.com>
2023-08-03 15:11:12 +0300

    bugFix

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,26 +1,30 @@
 /* preloader */
+if(typeof intervalVar === 'undefined' && typeof counter === 'undefined'){
+    let intervalVar = setInterval(preloaderInterval, 500);
+    let counter = 0;
+
+    function preloaderInterval() {
+        $('.preloaderTitle').text(() => {
+            counter++
+            counter > 2 ? counter = 0 : counter;
+            switch (counter){
+                case 0:
+                    return "Загрузка.";
+                case 1:
+                    return "Загрузка.."
+                case 2:
+                    return "Загрузка..."
+            }
+        })
+    }
+
+    $(window).on("load", function(){
+        clearInterval(intervalVar);
+        $('body').addClass('loaded_hiding');
+        window.setTimeout(function () {
+            $('body').addClass('loaded').removeClass('loaded_hiding');
+        }, 500);
+    });
+}
 
 
-/*var intervalVar = setInterval(preloaderInterval, 500);
-let counter = 0;
-function preloaderInterval() {
-    $('.preloaderTitle').text(() => {
-        counter++
-        counter > 2 ? counter = 0 : counter;
-        switch (counter){
-            case 0:
-                return "Загрузка.";
-            case 1:
-                return "Загрузка.."
-            case 2:
-                return "Загрузка..."
-        }
-    })
-}*/
-$(window).on("load", function(){
-    /*clearInterval(intervalVar);*/
-    $('body').addClass('loaded_hiding');
-    window.setTimeout(function () {
-        $('body').addClass('loaded').removeClass('loaded_hiding');
-    }, 500);
-});
